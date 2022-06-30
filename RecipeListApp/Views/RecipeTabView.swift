ac5a2c68cd031c3a36492059308825589e651370
calebcorrigan <caleb.corrigan@aol.com>
2022-06-30 18:29:34 -0400

    Build UI in the featureView

--- RecipeListApp/Views/RecipeTabView.swift
+++ RecipeListApp/Views/RecipeTabView.swift
@@ -14,25 +14,27 @@ struct RecipeTabView: View {
             
             RecipeFeatureView()
                 .tabItem {
-            VStack{
-                Image(systemName: "star.fill")
-                Text("Featured")
-            }
-        }
-        ContentView()
-            .tabItem{
-                VStack {
-                    Image(systemName: "list.bullet")
-                    Text("List")
+                    VStack {
+                        Image(systemName: "star.fill")
+                        Text("Featured")
+                    }
                 }
-            }
+            
+            ContentView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "list.bullet")
+                        Text("List")
+                    }
+                }
+        }
+        .environmentObject(RecipeModel())
         
-        } .environmentObject(RecipeModel())
+    }
 }
 
 struct RecipeTabView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeTabView()
     }
-}
 }
